aedc4d91269fce64ddab2a2202efbdcf921713f6
Joris <31243123+souped@users.noreply.github.com>
2020-12-06 14:26:33 +0100

    Add file

---
+++ 2020/aoc6.2020.py
@@ -0,0 +1,18 @@
+with open("AppData\\Local\\Programs\\Python\\Python37\\PythonCode\\Random_files\\inputsaoc\\input6.txt") as f:
+    a = [line.replace("\n", " ") for line in f.read().split("\n\n")]
+
+a1 = 0
+a2 = 0
+for l in [set(line.replace(" ", "")) for line in a]:
+    a1+=len(l)
+
+for l in [l.strip() for l in a]:
+    d = dict((el,0) for el in set(l.replace(" ", "")))
+    for c in l.replace(" ", ""):
+        d[c]+=1
+    f = len(l.split(" "))
+    e = [v for c,v in enumerate(d) if v == f]
+    if len(e) != 0 and f == max(e): a2+= len(e)
+
+
+print(a1,a2)
